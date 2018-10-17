017fb75496d46b148f3408f10afd283ed45bc9fc
Ville Kärkkäinen <ville.karkkainen@digia.com>
2018-10-17 15:03:41 +0300

    Bonobo pipeline with lock

---
+++ bonobo-pipelines-lock.py
@@ -0,0 +1,71 @@
+# Execute: bonobo run bonobo-lock.py
+
+import bonobo
+import logging
+import threading
+from bonobo.config import use
+import time
+
+@use('lock')
+def acquire_lock(lock):
+    logging.info('acquire_lock: START')
+    status = lock.acquire()
+    logging.info('acquire_lock: %s.' % status)
+    return status
+
+@use('lock')
+def release_lock(input, lock):
+    logging.info('release_lock: START')
+    lock.release()
+
+@use('lock')
+def a(status, lock):
+    logging.info('a')
+    return 42
+
+@use('lock')
+def b(id, lock):
+    logging.info('b: START')
+    while lock.locked():
+        time.sleep(2)
+    logging.info('b: %d' % id)
+
+def c(id):
+    logging.info('c: %d' % id)
+    return 'Hello'
+
+def d(name):
+    logging.info('d: %s' % name)
+    return name
+
+def get_graph(**options):
+
+    graph = bonobo.Graph()
+
+    graph.add_chain(
+        acquire_lock, 
+        a, 
+        b)
+    
+    graph.add_chain(
+        c,
+        d,
+        release_lock,
+        _input = a)
+
+    return graph
+
+def get_services(**options):
+
+    return {
+        'lock' : threading.Lock()
+    }
+
+# The __main__ block actually execute the graph.
+if __name__ == '__main__':
+    parser = bonobo.get_argument_parser()
+    with bonobo.parse_args(parser) as options:
+        bonobo.run(
+            get_graph(**options),
+            services=get_services(**options)
+        )
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Bonobo playground files
+
+## Bonobo parallel pipelines with lock
+
+[bonobo-pipelines-lock.py](bonobo-pipelines-lock.py) Experiment with Bonobo pipeline using lock. Node 'b' takes input from Node 'a' but is allowed to proceed only once pipeline with Nodes 'c' and 'd' have finished.
+
+![alt text](bonobo-pipelines-lock.png "Parallel pipelines with lock")
